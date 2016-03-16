38f446e26187bc8b9c3599611fa87ea20c72da66
ian650 <ian6502011@gmail.com>
2016-03-15 23:30:04 -0700

    Update and rename README.md to README.py

--- README.py
+++ README.py
@@ -1,6 +1,10 @@
 # Hello_World-
+animal = ['snake', 'rabbit','dog', 'dragon']
 Just another project 
 x = input("how old are you ")
 if (x = 16):
   Birthday_day= input("when is your birthday day?")
 def Animal_what():
+  if Birthday_day == 1:
+  print(animal[0])
+    
